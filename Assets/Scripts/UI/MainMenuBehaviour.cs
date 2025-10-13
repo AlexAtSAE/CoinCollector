1626c58e30a0bdc92f6acb49fd49f5a8cc279b23
AlexAtSAE <120990uk@saeinstitute.edu>
2025-10-13 12:45:42 +0100

    idk but its good

--- Assets/Scripts/UI/MainMenuBehaviour.cs
+++ Assets/Scripts/UI/MainMenuBehaviour.cs
@@ -3,13 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
+    public Canvas Options;
+    private Canvas thisCanvas;
+    public void Start()
+    {
+        thisCanvas = GetComponent<Canvas>();
+        thisCanvas.enabled = true;
+    }
     public void PressedPlay()
     {
         SceneManager.LoadScene("Game");
     }
     public void PressedOptions()
     {
-        Debug.Log("TODO");
+        Options.enabled = true;
+        thisCanvas.enabled = false;
     }
     public void PressedQuit()
     {
--- Assets/Scripts/UI/OptionsMenuBehaviour.cs
+++ Assets/Scripts/UI/OptionsMenuBehaviour.cs
@@ -8,13 +8,19 @@ public class OptionsMenuBehaviour : MonoBehaviour
     public UserSettings userSettings;
     public TextMeshProUGUI slideButtonText;
     public TextMeshProUGUI FOVText; public Slider FOVslider;
+    public TextMeshProUGUI XText; public Slider Xslider;
+    public TextMeshProUGUI YText; public Slider Yslider;
+    public Canvas MainMenu;
 
-    public void Update()
-    {
-
-    }
     private float timeout = 0;
     private bool changingSlide;
+
+    private Canvas thisCanvas;
+    public void Start()
+    {
+        thisCanvas = GetComponent<Canvas>();
+        thisCanvas.enabled = false;
+    }
     public void SetSlideButton()
     {
         changingSlide = true;
@@ -25,6 +31,30 @@ public class OptionsMenuBehaviour : MonoBehaviour
         userSettings.FOV = Res;
         FOVText.SetText($"{Res}");
     }
+    public void onPressBack()
+    {
+        MainMenu.enabled = true;
+        thisCanvas.enabled = false;
+    }
+
+    public void changeXSensitivity()
+    {
+        float val = Xslider.value;
+        float min = 10, max = 1000;
+        float res = Mathf.Lerp(min,max,val);
+        XText.SetText($"{Mathf.Floor(res)}");
+        userSettings.mouseSensitivity.x = res;
+    }
+    public void changeYSensitivity()
+    {
+        float val = Yslider.value;
+        float min = 10, max = 1000;
+        float res = Mathf.Lerp(min, max, val);
+        YText.SetText($"{Mathf.Floor(res)}");
+        userSettings.mouseSensitivity.y = res;
+    }
+
+
     public void OnGUI()
     {
         if (changingSlide && Event.current.type == EventType.KeyDown)
